0c969fca50082ba9ed68e7f8c6dca2ca13cbf0a4
olaniyi <arokoyueb11@gmail.com>
2024-10-22 01:39:27 +0100

    section development commit

--- src/app/Body/Section4/SectionFour.jsx
+++ src/app/Body/Section4/SectionFour.jsx
@@ -5,8 +5,28 @@ const SectionFour = () => {
   return (
     <div className='section_4_container'>
           <img src="images/blok.png" alt="blok" />
+
+          <div className='section_4_text_content'>
           <span className='section_four_hero_text'>Blok gifts you <br /> the most valuable <br /> asset in life - time.</span>
-    </div>
+
+          
+          
+              <button className='hero_btn'>Get Blok  &gt;</button>
+
+              <div class="profile-images">
+    <img className="profile-img" src="images/blok.png" alt="" />
+    <img className="profile-img" src="images/blok.png" alt="" />
+    <img className="profile-img" src="images/blok.png" alt="" />
+    <img className="profile-img" src="images/blok.png" alt="" />
+              </div>
+
+              <div className='comment_text_container'></div>
+
+              <span className='comment_text'>trusted by 1400+ <br /> high performing individuals</span>
+              
+              </div>
+          </div>
+          
   )
 }
 
